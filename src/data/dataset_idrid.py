95c1b5d296b04672e073708694354e2aad3d3d8b
Smridhi Patwari <smridhipatwari@Smridhis-MacBook-Pro-6.local>
2025-11-23 01:23:51 -0500

    Save: update src/data/dataset_idrid.py src/data/transforms.py

--- src/data/dataset_idrid.py
+++ src/data/dataset_idrid.py
@@ -1,5 +1,42 @@
-# dataset loader for IDRiD (placeholder)
+import os
+from PIL import Image
+from torch.utils.data import Dataset
 
-class IDRiDDataset:
-    def __init__(self, *args, **kwargs):
-        pass
+from .transforms import get_idrid_transforms
+
+class IdridSegmentationDataset(Dataset):
+    """
+    IDRiD segmentation dataset.
+    Expects:
+    - images_dir: folder with retinal images
+    - masks_dirs: dict with lesion_type -> folder containing corresponding masks
+    """
+
+    def __init__(self, images_dir: str, masks_dirs: dict, image_ids: list, phase: str = "train"):
+        self.images_dir = images_dir
+        self.masks_dirs = masks_dirs  # e.g. {"ma": path_to_ma_masks, "he": path_to_he_masks, ...}
+        self.image_ids = image_ids
+        self.phase = phase
+
+        self.transforms = get_idrid_transforms(phase)
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+
+        img_path = os.path.join(self.images_dir, f"{image_id}.jpg")  # adapt extension
+        image = Image.open(img_path).convert("RGB")
+
+        # Build a multi-channel mask (C x H x W)
+        masks = []
+        for lesion_type, lesion_dir in self.masks_dirs.items():
+            mask_path = os.path.join(lesion_dir, f"{image_id}.tif")  # adapt extension
+            mask = Image.open(mask_path).convert("L")
+            masks.append(mask)
+
+        if self.transforms is not None:
+            image, masks = self.transforms(image, masks)
+
+        return image, masks
--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,4 +1,26 @@
-# image transforms (placeholder)
+from torchvision import transforms
 
-def get_transforms():
-    return None
+def get_aptos_transforms(phase: str):
+    if phase == "train":
+        return transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+    else:
+        return transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+        ])
+
+def get_idrid_transforms(phase: str):
+    """
+    TODO: Replace with Albumentations pipeline that jointly augments image + masks.
+    For now, just resize.
+    """
+    def _simple_transform(image, masks):
+        image = image.resize((512, 512))
+        masks = [m.resize((512, 512)) for m in masks]
+        return image, masks
+
+    return _simple_transform
